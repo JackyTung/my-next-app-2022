ad8c8437db4dec1ed88c1bff09be9294717655ea
Jacky Tung <jacky.tung@langlive.com>
2022-04-09 10:36:20 +0800

    feat: init styled system

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,31 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { ThemeProvider } from "styled-components";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
+import Reset from "@/styles/reset";
+
+import themes from "@/styles/themes";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = () => {
+      window.scrollTo(0, 0);
+    };
+
+    router.events.on("routeChangeComplete", handleRouteChange());
+
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange());
+    };
+  }, [router.events]);
+  return (
+    <ThemeProvider theme={themes}>
+      <Reset />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
